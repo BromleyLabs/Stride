90373c9a4eeb3cc6cd0b051a2188545d323c90a7
Bon Filey <puneet@outdu.com>
2018-05-05 12:01:01 +0530

    Function added to sign a message.

--- scripts/utils.py
+++ scripts/utils.py
@@ -6,3 +6,16 @@ def checksum(addr):
         return w3.toChecksumAddress(addr)
     else:
         return addr
+
+def sign_bytearray(barray, account_adr):
+    # Returns hex strings like '0x3532..'
+    h = HexBytes(barray)
+    h_hash = w3.sha3(hexstr = h.hex())
+    sig = w3.eth.sign(account_adr, h_hash) # sig is HexBytes   
+    r = w3.toBytes(hexstr = HexBytes(sig[0 : 32]).hex())
+    s = w3.toBytes(hexstr = HexBytes(sig[32 : 64]).hex())
+    v = sig[64 : 65]
+    v_int = int.from_bytes(v, byteorder='big')
+    h_hash = w3.toBytes(hexstr = h_hash.hex())
+    return h_hash, v_int, r, s
+
