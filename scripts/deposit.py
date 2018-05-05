43365901e97c525a1e89222248978f17cc0fe361
Bon Filey <puneet@outdu.com>
2018-05-05 11:59:18 +0530

    Bug fixes in submit_ack method

--- scripts/deposit.py
+++ scripts/deposit.py
@@ -1,6 +1,5 @@
 from web3.auto import w3
 from hexbytes import HexBytes
-from solc import compile_source
 from web3.contract import ConciseContract
 import os
 from utils import *
@@ -11,19 +10,18 @@ ABI_FILE = os.path.join('../contracts/target/', CONTRACT_NAME + '.abi')
 BIN_FILE = os.path.join('../contracts/target/', CONTRACT_NAME + '.bin')
 OWNER = w3.eth.accounts[0]
 GAS = 4000000 
-GAS_PRICE = 5000000000
+GAS_PRICE = 10000000000
 
 # This class to be used once contract is deployed
 class RSKDepositContract:
     def __init__(self, addr, abi_file):
-        self.addr = addr
+        self.addr = checksum(addr)
         self.abi_file = abi_file
         abi = open(abi_file, 'rt').read()
-        addr = checksum(addr)
-        self.contract = w3.eth.contract(abi = abi, address = addr)
+        self.contract = w3.eth.contract(abi = abi, address = self.addr)
         self.concise = ConciseContract(self.contract)
 
-    def kill(self, contract_addr, abi_file, bin_file):
+    def kill(self):
         
         tx_hash = self.contract.functions.kill().transact({'from' : OWNER, 
                                            'gas' : GAS, 'gasPrice' : GAS_PRICE})
@@ -39,32 +37,23 @@ class RSKDepositContract:
                                     transact = {'from' : owner_addr,
                                     'gas' : GAS, 'gasPrice' : GAS_PRICE}) 
         print('Tx hash: %s' % HexBytes(tx_hash).hex())
-
         tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
         print (tx_receipt) 
 
-# This a separate function to be called only once.
-def deploy(contract_file, contract_name):
-    
-    compiled_sol = compile_source(open(contract_file, 'rt').read())
-    interface = compiled_sol['<stdin>:' + contract_name] 
-       
-    contract = w3.eth.contract(abi = interface['abi'], 
-                               bytecode = interface['bin'])
-    
-    tx_hash = contract.deploy(transaction = {'from' : OWNER, 'gas' : GAS, 
-                                             'gasPrice' : GAS_PRICE}) 
-    print('Tx hash: %s' % HexBytes(tx_hash).hex())
-
-    tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
-    print(tx_receipt)
-
+    def submit_ack(self, txn_id, ack_msg, account_addr):
+        # ack_msg is bytearray of 32 bytes, assumed for now
+        h_hash, v_int, r, s = sign_bytearray(ack_msg, account_addr)
+        tx_hash = self.contract.functions.submit_ack(txn_id, ack_msg, h_hash, v_int, r, s).transact({'from' : account_addr, 'gas' : GAS, 'gasPrice' : GAS_PRICE}) 
+                                         
+        print('Tx hash: %s' % HexBytes(tx_hash).hex())
+        tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
+        print (tx_receipt) 
 
 def main():
-    contract = RSKDepositContract('0x73067Ea0C3B4E23f3D73B7022c07C1FF4a7ca069',
-                                  ABI_FILE)
-
-    contract.add_custodian(w3.eth.accounts[2], w3.eth.accounts[0])
+    rsk = RSKDepositContract('0x73067Ea0C3B4E23f3D73B7022c07C1FF4a7ca069',
+                              ABI_FILE)
+    ack_msg = bytes(32)
+    rsk.submit_ack(0, ack_msg, w3.eth.accounts[2])
 
 if __name__== '__main__':
     main()
