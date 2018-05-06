50954f3d341ea6a7f76a55f03556dc8c85bd64ca
Bon Filey <puneet@outdu.com>
2018-05-06 07:03:14 +0530

    Msg format changed.  Checks for msgs added. WIP.

--- contracts/rsk_deposit_contract.sol
+++ contracts/rsk_deposit_contract.sol
@@ -26,23 +26,25 @@ contract RSKDepositContract is mortal {
     struct ForwardTxn { /* from SBTC -> EBTC */
         uint txn_id;
         address user;
-        uint amount;  /* SBTC */ 
+        uint stbc_amount;  /* SBTC */ 
         address eth_addr;  
         TxnStates state; 
-        bytes32 ack_msg; /* TODO: has to be longer. from custodian */
+        bytes ack_msg; /* TODO: has to be longer. from custodian */
         bytes32 ack_msg_hash; 
         uint8 v; /* Signed by custodian */
         bytes32 r;
         bytes32 s;
     }
 
-    uint private m_txn_count = 0;
     address public m_custodian; 
     address constant m_sbtc_token_addr = 0xc778417E063141139Fce010982780140Aa0cD5Ab; /* WETH for testing */ 
     mapping (uint => ForwardTxn) public m_txns; 
 
+    uint private m_txn_count = 1; /* We will use 0 to check existence of key in above mapping */
+                                  /* TODO: Is there a need to use a hash as an id? */
+
     event Deposited(address from, address to, uint amount, uint txn_id);
-    event Ack(bytes32 json_msg, bytes32 signature, uint8 v, bytes32 r, bytes32 s);
+    event Ack(bytes ack_msg, bytes32 ack_msg_hash, uint8 v, bytes32 r, bytes32 s);
 
     function add_custodian(address addr) public {
         require(msg.sender == m_owner, "Only owner can call this");  
@@ -71,23 +73,42 @@ contract RSKDepositContract is mortal {
 
     }
 
-    /* ack_msg: "{fromSbtc: <>, toEthr: <>, amount : <>, blocNumber: <>}"
+    /* Extract bytes32 from a byte array given an offset */
+    function to_bytes32(bytes b, uint offset) private pure returns (bytes32) {
+        bytes32 out;
+
+        for (uint i = 0; i < 32; i++) 
+            out |= bytes32(b[offset + i] & 0xFF) >> (i * 8);
+
+        return out;
+    }
+
+    /* ack_msg byte array: tx_id(32), fromSbtc(32), toEthr(32), amount(32), blocNumber(32)
        txn_id is what custodian reads from Deposited event */
-    
-    function submit_ack(uint txn_id, bytes32 ack_msg, bytes32 ack_msg_hash, 
+    /* TODO: ack_msg can be packed better to save memory if it really matters */ 
+    function submit_ack(bytes ack_msg, bytes32 ack_msg_hash, 
                         uint8 v, bytes32 r, bytes32 s) public { /* Sent by custodian */
         require(msg.sender == m_custodian, "Only custodian can call this");
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         bytes32 prefixedHash = keccak256(prefix, ack_msg_hash) ; 
         require(ecrecover(prefixedHash, v, r, s) == m_custodian, "Not a custodian signed msg"); 
-         
-        m_txns[txn_id].ack_msg = ack_msg; 
+        require(ack_msg_hash == keccak256(ack_msg), "Ack msg hash does not match"); 
+       
+        /* TODO: Verify if custodian has sent correct information for this transaction id */
+        uint txn_id = uint(to_bytes32(ack_msg, 0));
+        require(m_txns[txn_id].txn_id != 0, "Txn id does not exist");
+        
+        address user = address(to_bytes32(ack_msg, 32));
+        require(user == m_txns[txn_id].user);
+
+        m_txns[txn_id].ack_msg = ack_msg;  /* TODO: Check copy/reference during assignment */
         m_txns[txn_id].ack_msg_hash = ack_msg_hash; 
         m_txns[txn_id].v = v;
         m_txns[txn_id].r = r;
         m_txns[txn_id].s = s;
         m_txns[txn_id].state = TxnStates.ACKNOWLEDGED; 
 
-        emit Ack(ack_msg, ack_msg_hash, v, r, s); /* User watches this event */
+        emit Ack(ack_msg, ack_msg_hash, v, r, s); /* User watches this event and verifies info is correct */
+        /* TODO: What does user do if the info here is not correct? */
     }
 }
