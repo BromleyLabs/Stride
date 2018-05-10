b68b565e8e17ad87fc844b8d5baea37a9379a4b0
Bon Filey <puneet@outdu.com>
2018-05-10 13:45:58 +0530

    Compiles fine. WIP.

--- contracts/user_rsk.sol
+++ contracts/user_rsk.sol
@@ -1,5 +1,4 @@
-pragma solidiy ^0.4.23;
-
+pragma solidity ^0.4.23;
 
 contract ERC20Interface {
     function totalSupply() public constant returns (uint);
@@ -21,45 +20,52 @@ contract mortal {
 
 contract UserRSKContract is mortal {
 
+    enum TxnStates {CREATED, EXECUTED}
+
     struct ForwardTxn {
         address user;
         bytes32 txn_id; 
         address custodian; 
         bytes32 custodian_pwd_hash; /* Custodian password hash */
         uint timeout_interval; /* Blocks. Arbitary */ 
-        uint current_block; 
+        uint creation_block; 
         uint sbtc_amount;
-    }; 
+        TxnStates state;
+    } 
 
     mapping (bytes32 => ForwardTxn) public m_txns; 
     event UserTransactionCreated(bytes32 txn_id, address user, address custodian);
+    event CustodianExecutionSuccess(bytes32 txn_id, bytes pwd_str); 
 
     address constant m_sbtc_token_addr = 0xc778417E063141139Fce010982780140Aa0cD5Ab; /* WETH for testing */ 
 
     function create_transaction(bytes32 txn_id, address custodian, bytes32 custodian_pwd_hash, 
-                                uint timeout_interval, uint sbtc_amount public {
+                                uint timeout_interval, uint sbtc_amount) public {
         /* Assumed user has generated a unique txn_id.  May not matter, but just to avoid unnecessary
            handling and potential problems */
        
         m_txns[txn_id] = ForwardTxn(msg.sender, txn_id, custodian, custodian_pwd_hash, timeout_interval,
-                                    block.number, sbtc_amount);
+                                    block.number, sbtc_amount, TxnStates.CREATED);
         
         emit UserTransactionCreated(txn_id, msg.sender, custodian);
     }
 
 
-    function execute(bytes32 txn_id, bytes input_str) public { /* Called by custodian */
-         require(msg.sender == m_custodian, "Only custodian can call this"); 
+    function execute(bytes32 txn_id, bytes pwd_str) public { /* Called by custodian */
+         
+        require(msg.sender == m_txns[txn_id].custodian, "Only custodian can call this"); 
+        require(m_txns[txn_id].state == TxnStates.CREATED, "Transaction already executed");
 
-        /* swap logic.  Assumption here is that user has already deposited SBTC to this contract */
-            
+        /* Atomic swap logic.  Assumption here is that user has already deposited SBTC to this contract */
         ERC20Interface token_contract = ERC20Interface(m_sbtc_token_addr);
-        if(block.number > m_timeout_block) {
-            require(token_contract.transferFrom(this, m_owner, sbtc_amount);
+        if(block.number > (m_txns[txn_id].creation_block + m_txns[txn_id].timeout_interval)) {
+            require(token_contract.transferFrom(this, m_owner, m_txns[txn_id].sbtc_amount));
         }
         else {
-            require(m_custodian_pwd_hash == keccak256(input_str), "Hash does not match");
-            require(token_contract.transferFrom(this, m_custodian, sbtc_amount);
+            require(m_txns[txn_id].custodian_pwd_hash == keccak256(pwd_str), "Hash does not match");
+            require(token_contract.transferFrom(this, m_txns[txn_id].custodian, m_txns[txn_id].sbtc_amount));
+            m_txns[txn_id].state = TxnStates.EXECUTED;
+            emit CustodianExecutionSuccess(txn_id, pwd_str);
         }
     }
 }
