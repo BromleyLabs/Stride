0088b522f428ae9dbf050d1b9bc91079baa8e500
Bon Filey <puneet@outdu.com>
2018-05-12 11:16:14 +0530

    Refund function separated.

--- contracts/custodian_eth.sol
+++ contracts/custodian_eth.sol
@@ -11,7 +11,7 @@ contract mortal {
 
 contract CustodianEthContract is mortal {
 
-    enum TxnStates {CREATED, EXECUTED}
+    enum TxnStates {UNINITIALIZED, CREATED, EXECUTED, REFUNDED}
 
     struct ForwardTxn {
         address custodian_eth;  /* Eth address */
@@ -30,6 +30,7 @@ contract CustodianEthContract is mortal {
     event CustodianTransactionCreated(uint txn_id, address custodian_eth, address user_eth);
     event CustodianTransferred(uint txn_id);
     event UserExecutionSuccess(uint txn_id);
+    event RefundedToCustodian(uint txn_id);
 
     function create_transaction(uint txn_id, address user_eth, bytes32 custodian_pwd_hash, 
                                 uint timeout_interval, uint ebtc_amount) public {
@@ -52,24 +53,31 @@ contract CustodianEthContract is mortal {
         emit CustodianTransferred(txn_id);
     }
 
-    /* TODO: Separate the two parts below into separate function calls */
-    function execute(uint txn_id, string pwd_str) public { /* Called by user */ 
-        require(msg.sender == m_txns[txn_id].user_eth, "Only user can call this"); 
-        require(m_txns[txn_id].state == TxnStates.CREATED, "Transaction already executed");
-
+    function refund(uint txn_id) public { /* Called by custodian */
         ForwardTxn memory txn = m_txns[txn_id]; /* Convenience. TODO: Check if this is reference or a copy */
+        require(msg.sender == txn.custodian_eth, "Only custodian can call this"); 
+        require(txn.state == TxnStates.CREATED, "Transaction not in CREATED state"); 
+        require(block.number > (txn.creation_block + txn.timeout_interval));
 
-        /* Atomic swap logic.  Assumption here is that custodian has already deposited EBTC to this contract */
         ERC20Interface token_contract = ERC20Interface(m_ebtc_token_addr);
-        if(block.number > (txn.creation_block + txn.timeout_interval)) {
-            require(token_contract.transferFrom(this, txn.custodian_eth, txn.ebtc_amount));
-        }
-        else {
-            require(txn.custodian_pwd_hash == keccak256(pwd_str), "Hash does not match");
-            require(token_contract.transferFrom(this, txn.user_eth, txn.ebtc_amount)); 
-            txn.state = TxnStates.EXECUTED;
-            emit UserExecutionSuccess(txn_id);
-        }
+        require(token_contract.transferFrom(this, txn.custodian_eth, txn.ebtc_amount)); 
+        txn.state = TxnStates.REFUNDED;
+
+        emit RefundedToCustodian(txn_id);
+    }
+
+    function execute(uint txn_id, string pwd_str) public { /* Called by user */
+        ForwardTxn memory txn = m_txns[txn_id]; 
+        require(msg.sender == txn.user_eth, "Only user can call this"); 
+        require(txn.state == TxnStates.CREATED, "Transaction not in CREATED state");
+        require(block.number <= (txn.creation_block + txn.timeout_interval));
+        require(txn.custodian_pwd_hash == keccak256(pwd_str), "Hash does not match");
+
+        ERC20Interface token_contract = ERC20Interface(m_ebtc_token_addr);
+        require(token_contract.transferFrom(this, txn.user_eth, txn.ebtc_amount));
+        txn.state = TxnStates.EXECUTED;
+
+        emit UserExecutionSuccess(txn_id);
     }
 }
 
--- contracts/user_rsk.sol
+++ contracts/user_rsk.sol
@@ -11,7 +11,7 @@ contract mortal {
 
 contract UserRSKContract is mortal {
 
-    enum TxnStates {CREATED, EXECUTED}
+    enum TxnStates {UNINITIALIZED, CREATED, EXECUTED, REFUNDED}
 
     struct ForwardTxn {
         address user_rsk; /* RSK address */
@@ -30,6 +30,7 @@ contract UserRSKContract is mortal {
     event UserTransactionCreated(uint txn_id, address user_rsk, address custodian_rsk);
     event UserTransferred(uint txn_id);
     event CustodianExecutionSuccess(uint txn_id, string pwd_str); 
+    event RefundedToUser(uint txn_id);
 
     function create_transaction(uint txn_id, address custodian_rsk, bytes32 custodian_pwd_hash, 
                                 uint timeout_interval, uint sbtc_amount) public {
@@ -52,24 +53,31 @@ contract UserRSKContract is mortal {
         emit UserTransferred(txn_id);
     }
 
-    /* TODO: Separate the two parts below into separate function calls */
-    function execute(uint txn_id, string pwd_str) public { /* Called by custodian */
-        require(msg.sender == m_txns[txn_id].custodian_rsk, "Only custodian can call this"); 
-        require(m_txns[txn_id].state == TxnStates.CREATED, "Transaction already executed");
-
+    function refund(uint txn_id) public { /* Called by user */
         ForwardTxn memory txn = m_txns[txn_id]; /* Convenience. TODO: Check if this is reference or a copy */
+        require(msg.sender == txn.user_rsk, "Only user can call this"); 
+        require(txn.state == TxnStates.CREATED, "Transaction not in CREATED state"); 
+        require(block.number > (txn.creation_block + txn.timeout_interval));
 
-        /* Atomic swap logic.  Assumption here is that user has already deposited SBTC to this contract */
         ERC20Interface token_contract = ERC20Interface(m_sbtc_token_addr);
-        if(block.number > (txn.creation_block + txn.timeout_interval)) {
-            require(token_contract.transferFrom(this, txn.user_rsk, txn.sbtc_amount)); 
-        }
-        else {
-            require(txn.custodian_pwd_hash == keccak256(pwd_str), "Hash does not match");
-            require(token_contract.transferFrom(this, txn.custodian_rsk, txn.sbtc_amount));
-            txn.state = TxnStates.EXECUTED;
-            emit CustodianExecutionSuccess(txn_id, pwd_str);
-        }
+        require(token_contract.transferFrom(this, txn.user_rsk, txn.sbtc_amount)); 
+        txn.state = TxnStates.REFUNDED;
+
+        emit RefundedToUser(txn_id);
+    }
+
+    function execute(uint txn_id, string pwd_str) public { /* Called by custodian */
+        ForwardTxn memory txn = m_txns[txn_id]; 
+        require(msg.sender == txn.custodian_rsk, "Only custodian can call this"); 
+        require(txn.state == TxnStates.CREATED, "Transaction not in CREATED state");
+        require(block.number <= (txn.creation_block + txn.timeout_interval));
+        require(txn.custodian_pwd_hash == keccak256(pwd_str), "Hash does not match");
+
+        ERC20Interface token_contract = ERC20Interface(m_sbtc_token_addr);
+        require(token_contract.transferFrom(this, txn.custodian_rsk, txn.sbtc_amount));
+        txn.state = TxnStates.EXECUTED;
+
+        emit CustodianExecutionSuccess(txn_id, pwd_str);
     }
 }
 
