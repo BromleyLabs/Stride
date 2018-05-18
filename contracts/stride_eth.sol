fbdc8a845ecb865243157285a269798dc726dfdf
Bon Filey <puneet@outdu.com>
2018-05-18 19:48:18 +0530

    WIP.

--- contracts/stride_eth.sol
+++ contracts/stride_eth.sol
@@ -9,10 +9,10 @@ import "mortal.sol";
 contract ForwardEthContract is mortal {
     using SafeMath for uint;
 
-    enum TxnStates {UNINITIALIZED, CREATED, EXECUTED, REFUNDED}
+    enum FwdTxnStates {UNINITIALIZED, CREATED, EXECUTED, REFUNDED}
+    enum RevTxnStates {UNINITIALIZED, DEPOSITED, ACKNOWLEDGED, REVERTED}
 
-    struct ForwardTxn {
-        address custodian_eth;  /* Eth address */
+    struct ForwardTxn { /* from SBTC -> EBTC */
         uint txn_id; 
         address user_eth; 
         bytes32 custodian_pwd_hash; /* Custodian password hash */
@@ -20,19 +20,47 @@ contract ForwardEthContract is mortal {
         uint creation_block; 
         uint ebtc_amount; 
         uint collateral_eth; /* Calculated */
-        TxnStates state;
+        FwdTxnStates state;
     }  /* TODO: What is the cost of this storage? */
 
-    mapping (uint => ForwardTxn) public m_txns; 
+    struct ReverseTxn { /* from EBTC -> SBTC */
+        uint txn_id;
+        address user_eth;
+        address user_rsk;
+        uint ebtc_amount;  
+        RevTxnStates state; 
+        uint block_number; /* When created */
+        bytes ack_msg; /* From custodian */ 
+        bytes32 ack_msg_hash; 
+        uint8 v; /* Signed by custodian */
+        bytes32 r;
+        bytes32 s;
+    }
+
+    mapping (uint => ForwardTxn) public m_fwd_txns; 
+    mapping (uint => ReverseTxn) public m_rev_txns; 
+
+    address m_custodian_eth;
     address m_ebtc_token_addr; /* Set by method below */ 
     uint m_eth_ebtc_ratio_numerator = 15; 
     uint m_eth_ebtc_ratio_denominator = 1;
+    uint public m_penalty; /* Custodian penalty in Eth for reverse txn for not sending ack to user */ 
+    uint m_max_ack_delay = 200; /* In blocks. Ack has to be sent by custodian within so many block numbers */
 
-    event CustodianTransactionCreated(uint txn_id, address custodian_eth, address user_eth);
-    event CustodianTransferred(uint txn_id);
-    event UserExecutionSuccess(uint txn_id);
-    event RefundedToCustodian(uint txn_id);
+    event FwdCustodianTransactionCreated(uint txn_id, address m_custodian_eth, address user_eth);
+    event FwdCustodianTransferred(uint txn_id);
+    event FwdUserExecutionSuccess(uint txn_id);
+    event FwdRefundedToCustodian(uint txn_id);
    
+    event RevUserDeposited(address from, uint amount, uint txn_id);
+    event RevCustodianAck(bytes ack_msg, bytes32 ack_msg_hash, uint8 v, bytes32 r, bytes32 s);
+    event RevChallengeAccepted(uint txn_id);
+ 
+    function set_custodian(address addr) public {
+        require(msg.sender == m_owner);
+        m_custodian_eth = addr;
+    }
+
     function set_ebtc_token_address(address addr) public {
         require(msg.sender == m_owner, "Only owner can set this");
         m_ebtc_token_addr = addr; 
@@ -44,48 +72,147 @@ contract ForwardEthContract is mortal {
         m_eth_ebtc_ratio_denominator = denominator;
     }
 
-    function create_transaction(uint txn_id, address user_eth, bytes32 custodian_pwd_hash, 
-                                uint timeout_interval, uint ebtc_amount) public {
-        require(m_txns[txn_id].txn_id != txn_id, "Transaction already exists");
-       
+    function set_ebtc_token_addr(address addr) public {
+        require(msg.sender == m_owner);
+        m_ebtc_token_addr = addr;
+    }
+
+    function set_penalty(uint amount) public {
+        require(msg.sender == m_owner, "Only owner can set penalty");  
+        m_penalty = amount;
+    }
+
+    function set_max_ack_delay(uint nblocks) public {
+        require(msg.sender == m_owner, "Only owner can set max_ack_delay");  
+        m_max_ack_delay = nblocks; 
+    }
+
+    /* Utility function to extract bytes from a byte array given an offset and returns as bytes32, bytes20 */
+    function get_bytes32(bytes b, uint offset) private pure returns (bytes32) {
+        bytes32 out;
+        for (uint i = 0; i < 32; i++) 
+            out |= bytes32(b[offset + i] & 0xFF) >> (i * 8); /* CAUTION: Beware of left/right padding from bytes->bytes32  */
+        return out;
+    }
+
+    function get_bytes20(bytes b, uint offset) private pure returns (bytes20) {
+        bytes20 out;
+        for (uint i = 0; i < 20; i++) 
+            out |= bytes20(b[offset + i] & 0xFF) >> (i * 8); 
+        return out;
+    }
+    /* With this method custodian is initializing the transaction along with sending Eth to 
+       contract */
+    function fwd_create_transaction(uint txn_id, address user_eth, bytes32 custodian_pwd_hash, 
+                                uint timeout_interval, uint ebtc_amount) public payable { /* Called by custodian */
+        require(m_fwd_txns[txn_id].txn_id != txn_id, "Transaction already exists");
+        require(msg.sender == m_custodian_eth);
         uint collateral_eth = (ebtc_amount.mul(m_eth_ebtc_ratio_numerator)).div(m_eth_ebtc_ratio_denominator); 
-        m_txns[txn_id] = ForwardTxn(msg.sender, txn_id, user_eth, custodian_pwd_hash, timeout_interval,
-                                    block.number, ebtc_amount, collateral_eth, TxnStates.CREATED);
+        require(msg.value == collateral_eth); 
+
+        m_fwd_txns[txn_id] = ForwardTxn(txn_id, user_eth, custodian_pwd_hash, timeout_interval,
+                                    block.number, ebtc_amount, collateral_eth, FwdTxnStates.CREATED);
         
-        emit CustodianTransactionCreated(txn_id, msg.sender, user_eth);
+        emit FwdCustodianTransactionCreated(txn_id, msg.sender, user_eth);
+        emit FwdCustodianTransferred(txn_id); /* TODO: this event may not be needed */
     }
 
-    function transfer_to_contract(uint txn_id) public payable { /* To be called by custodian to deposit Eth */
-        ForwardTxn memory txn = m_txns[txn_id]; /* Convenience. TODO: Check if this is reference or a copy */
-        require(msg.sender == txn.custodian_eth);
-        require(txn.txn_id == txn_id, "Transaction does not exist"); 
-        require(msg.value == txn.collateral_eth); 
-     
-        emit CustodianTransferred(txn_id);
-    }
-
-    function request_refund(uint txn_id) public { /* Called by custodian */
-        ForwardTxn memory txn = m_txns[txn_id]; 
-        require(msg.sender == txn.custodian_eth, "Only custodian can call this"); 
-        require(txn.state == TxnStates.CREATED, "Transaction not in CREATED state"); 
+    function fwd_request_refund(uint txn_id) public { /* Called by custodian */
+        ForwardTxn memory txn = m_fwd_txns[txn_id]; 
+        require(msg.sender == m_custodian_eth, "Only custodian can call this"); 
+        require(txn.state == FwdTxnStates.CREATED, "Transaction not in CREATED state"); 
         require(block.number > (txn.creation_block + txn.timeout_interval));
-        txn.custodian_eth.transfer(txn.collateral_eth);
-        txn.state = TxnStates.REFUNDED;
+        m_custodian_eth.transfer(txn.collateral_eth);
+        txn.state = FwdTxnStates.REFUNDED;
 
-        emit RefundedToCustodian(txn_id);
+        emit FwdRefundedToCustodian(txn_id);
     }
 
-    function execute(uint txn_id, string pwd_str) public { /* Called by user */
-        ForwardTxn memory txn = m_txns[txn_id]; 
+    function fwd_execute(uint txn_id, string pwd_str) public { /* Called by user */
+        ForwardTxn memory txn = m_fwd_txns[txn_id]; 
         require(msg.sender == txn.user_eth, "Only user can call this"); 
-        require(txn.state == TxnStates.CREATED, "Transaction not in CREATED state");
+        require(txn.state == FwdTxnStates.CREATED, "Transaction not in CREATED state");
         require(block.number <= (txn.creation_block + txn.timeout_interval));
         require(txn.custodian_pwd_hash == keccak256(pwd_str), "Hash does not match");
 
         require(EBTCToken(m_ebtc_token_addr).issueFreshTokens(txn.user_eth, txn.ebtc_amount));
-        txn.state = TxnStates.EXECUTED;
+        txn.state = FwdTxnStates.EXECUTED;
 
-        emit UserExecutionSuccess(txn_id);
+        emit FwdUserExecutionSuccess(txn_id);
+    }
+
+    function rev_redeem_ebtc(uint txn_id, uint ebtc_amount, address user_rsk_addr) public { /* By user */
+        /* Fresh txn_id generated by user */
+        require(m_rev_txns[txn_id].txn_id != txn_id, "Transaction already exists");
+
+        /* TODO: Do we need to ensure that custodian has deposited enough Eth for any penality when challenge is made by user */
+        require(EBTCToken(m_ebtc_token_addr).transferFrom(msg.sender, this, ebtc_amount));
+        require(EBTCToken(m_ebtc_token_addr).burnTokens(ebtc_amount));
+
+        m_rev_txns[txn_id] = ReverseTxn(txn_id, msg.sender, user_rsk_addr, ebtc_amount, 
+                                    RevTxnStates.DEPOSITED, block.number, "", 0, 0, 0, 0); 
+
+        emit RevUserDeposited(msg.sender, ebtc_amount, txn_id); /* Custodian watches this event */ 
+    }
+
+    /* This function is called by custodian after receiving Deposited event.
+       ack_msg byte array: txn_id(32), user_eth(20), user_rsk(20), ebtc_amount(32), block_number(32) */
+
+    function rev_submit_ack(bytes ack_msg, bytes32 ack_msg_hash, uint8 v, bytes32 r, bytes32 s) public {
+        require(msg.sender == m_custodian_eth, "Only custodian can call this");
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHash = keccak256(prefix, ack_msg_hash) ; 
+        require(ecrecover(prefixedHash, v, r, s) == m_custodian_eth, "Not a custodian signed msg"); 
+        require(ack_msg_hash == keccak256(ack_msg), "Ack msg hash does not match"); 
+       
+        /* Verify if custodian has sent correct information for this transaction id */
+        uint offset = 0;
+        uint txn_id = uint(get_bytes32(ack_msg, offset));
+        require(m_rev_txns[txn_id].txn_id != 0, "Txn id does not exist");
+
+        ReverseTxn memory txn = m_rev_txns[txn_id];  
+
+        /* Avoid repeated ack message for the same txn_id */
+        require(txn.state == RevTxnStates.DEPOSITED, "Txn not in DEPOSITED state");
+        
+        offset += 32;
+        address user = address(get_bytes20(ack_msg, offset));
+        require(user == txn.user_eth, "User does not match");
+
+        offset += 20;
+        address user_rsk = address(get_bytes20(ack_msg, offset));
+        require(user_rsk == txn.user_rsk, "User RSK address does not match");
+
+        offset += 20;
+        uint ebtc_amount = uint(get_bytes32(ack_msg, offset));
+        require(ebtc_amount == txn.ebtc_amount, "EBTC amount does not match");
+  
+        /* TODO: Check if ack has come too late, i.e. block number limit has been exceeded */
+
+        /* Save the ack info for further use by this contract */
+        txn.ack_msg = ack_msg;  /* TODO: Check copy/reference during assignment */
+        txn.ack_msg_hash = ack_msg_hash; 
+        txn.v = v;
+        txn.r = r;
+        txn.s = s;
+        txn.state = RevTxnStates.ACKNOWLEDGED; 
+
+        emit RevCustodianAck(ack_msg, ack_msg_hash, v, r, s); /* User watches this event and verifies info is correct */
+        /* TODO: What does user do if the info here is not correct? */
+    }
+
+    /* This function is called by user if user does not receive Ack event after m_max_ack_delay blocks. */
+    function no_ack_challenge(uint txn_id) public {
+        require(m_rev_txns[txn_id].txn_id != 0, "Txn id does not exist");
+        ReverseTxn memory txn = m_rev_txns[txn_id];
+        require(txn.user_eth == msg.sender, "User does not match");
+        require(txn.state == RevTxnStates.DEPOSITED, "Txn has not in DEPOSITED state"); 
+        require((block.number - txn.block_number) > m_max_ack_delay, "Not reached block limit"); 
+
+        /* Give back the user all the EBTC that were deposited */
+        require(EBTCToken(m_ebtc_token_addr).issueFreshTokens(txn.user_eth, txn.ebtc_amount)); /* Since they have been burnt already */
+        txn.state = RevTxnStates.REVERTED;
+        emit RevChallengeAccepted(txn_id);
     }
 }
 
--- contracts/stride_rsk.sol
+++ contracts/stride_rsk.sol
@@ -12,7 +12,6 @@ contract UserRSKContract is mortal {
     struct ForwardTxn {
         address user_rsk; /* RSK address */
         uint txn_id; 
-        address custodian_rsk;  
         bytes32 custodian_pwd_hash; /* Custodian password hash */
         uint timeout_interval; /* Blocks. Arbitary */ 
         uint creation_block; 
@@ -21,23 +20,29 @@ contract UserRSKContract is mortal {
     } 
 
     mapping (uint => ForwardTxn) public m_txns; 
+    address m_custodian_rsk;   
 
     event UserTransactionCreated(uint txn_id, address user_rsk, address custodian_rsk);
     event UserTransferred(uint txn_id);
     event CustodianExecutionSuccess(uint txn_id, string pwd_str); 
     event RefundedToUser(uint txn_id);
 
-    function create_transaction(uint txn_id, address custodian_rsk, bytes32 custodian_pwd_hash, 
-                                uint timeout_interval, uint sbtc_amount) public {
-        require(m_txns[txn_id].txn_id != txn_id, "Transaction already exists");
-       
-        m_txns[txn_id] = ForwardTxn(msg.sender, txn_id, custodian_rsk, custodian_pwd_hash, timeout_interval,
-                                    block.number, sbtc_amount, TxnStates.CREATED);
-        
-        emit UserTransactionCreated(txn_id, msg.sender, custodian_rsk);
+    function set_custodian(address addr) public {
+        require(msg.sender == m_owner);
+        m_custodian_rsk = addr;
     }
 
-    function transfer_to_contract(uint txn_id) public payable { /* To be called by user */
+    function fwd_create_transaction(uint txn_id, bytes32 custodian_pwd_hash, 
+                                uint timeout_interval, uint sbtc_amount) public { /* By user */
+        require(m_txns[txn_id].txn_id != txn_id, "Transaction already exists");
+       
+        m_txns[txn_id] = ForwardTxn(msg.sender, txn_id, custodian_pwd_hash, timeout_interval,
+                                    block.number, sbtc_amount, TxnStates.CREATED);
+        
+        emit UserTransactionCreated(txn_id, msg.sender, m_custodian_rsk);
+    }
+
+    function fwd_transfer_to_contract(uint txn_id) public payable { /* To be called by user */
         ForwardTxn memory txn = m_txns[txn_id]; /* Convenience. TODO: Check if this is reference or a copy */
         require(msg.sender == txn.user_rsk);
         require(txn.txn_id == txn_id, "Transaction does not exist"); 
@@ -46,7 +51,7 @@ contract UserRSKContract is mortal {
         emit UserTransferred(txn_id);
     }
 
-    function request_refund(uint txn_id) public { /* Called by user */
+    function fwd_request_refund(uint txn_id) public { /* Called by user */
         ForwardTxn memory txn = m_txns[txn_id]; 
         require(msg.sender == txn.user_rsk, "Only user can call this"); 
         require(txn.state == TxnStates.CREATED, "Transaction not in CREATED state"); 
@@ -58,19 +63,25 @@ contract UserRSKContract is mortal {
         emit RefundedToUser(txn_id);
     }
 
-    function execute(uint txn_id, string pwd_str) public { /* Called by custodian */
+    /* This function main job is to send password string to user */ 
+    function fwd_send_pwd(uint txn_id, string pwd_str) public { /* Called by custodian */
         ForwardTxn memory txn = m_txns[txn_id]; 
-        require(msg.sender == txn.custodian_rsk, "Only custodian can call this"); 
+        require(msg.sender == m_custodian_rsk, "Only custodian can call this"); 
         require(txn.state == TxnStates.CREATED, "Transaction not in CREATED state");
         require(block.number <= (txn.creation_block + txn.timeout_interval));
         require(txn.custodian_pwd_hash == keccak256(pwd_str), "Hash does not match");
-
-        txn.custodian_rsk.transfer(txn.sbtc_amount); 
 
         txn.state = TxnStates.EXECUTED;
 
         emit CustodianExecutionSuccess(txn_id, pwd_str);
     }
+
+    function rev_execute(address to, uint sbtc_amount) public { /* By custodian */
+        require(msg.sender == m_custodian_rsk, "Only custodian can call this"); 
+        /* TODO: We need to check if the amount is going to correct user */
+        to.transfer(sbtc_amount);
+    }
+
 }
 
 
