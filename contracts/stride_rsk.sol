070dfdefc991dbd73ac21f528f995c6144c90ecf
Bon Filey <puneet@outdu.com>
2018-05-19 19:05:52 +0530

    Complete revamp based on latest specs. WIP.

--- contracts/stride_rsk.sol
+++ contracts/stride_rsk.sol
@@ -4,28 +4,49 @@ pragma solidity ^0.4.23;
 
 import "erc20.sol";
 import "mortal.sol";
+import "safe_math.sol";
 
-contract UserRSKContract is mortal {
+contract StrideRSKContract is mortal {
+    using SafeMath for uint;
 
-    enum TxnStates {UNINITIALIZED, CREATED, EXECUTED, REFUNDED}
+    enum FwdTxnStates {UNINITIALIZED, CREATED, TRANSFERRED, EXECUTED, REFUNDED}
+    enum RevTxnStates {UNINITIALIZED, DEPOSITED, TRANSFERRED, CUSTODIAN_CHALLENGE_ACCEPTED}  
 
     struct ForwardTxn {
-        address user_rsk; /* RSK address */
         uint txn_id; 
+        address user_rsk; /* RSK address */
         bytes32 custodian_pwd_hash; /* Custodian password hash */
         uint timeout_interval; /* Blocks. Arbitary */ 
         uint creation_block; 
         uint sbtc_amount;
-        TxnStates state;
+        FwdTxnStates state;
     } 
 
-    mapping (uint => ForwardTxn) public m_txns; 
-    address m_custodian_rsk;   
+    struct ReverseTxn {
+        uint txn_id; 
+        address user_rsk;
+        address dest_rsk_addr;
+        bytes32 ack_hash; 
+        uint sbtc_amount;
+        uint creation_block;
+        RevTxnStates state;
+    }
 
-    event UserTransactionCreated(uint txn_id, address user_rsk, address custodian_rsk);
-    event UserTransferred(uint txn_id);
-    event CustodianExecutionSuccess(uint txn_id, string pwd_str); 
-    event RefundedToUser(uint txn_id);
+    mapping (uint => ForwardTxn) public m_fwd_txns;  /* TODO: Efficient and cheaper storage and purge strategy of transactions */ 
+    mapping (uint => ReverseTxn) public m_rev_txns; 
+
+    address m_custodian_rsk;   
+    uint m_locked_sbtc = 0;
+    uint m_sbtc_lock_interval = 100;  /* In blocks */
+
+    event FwdUserTransactionCreated(uint txn_id, address user_rsk, address custodian_rsk);
+    event FwdUserTransferred(uint txn_id);
+    event FwdCustodianExecutionSuccess(uint txn_id, string pwd_str); 
+    event FwdRefundedToUser(uint txn_id);
+
+    event RevCustodianDeposited(uint txn_id, bytes32 ack_hash); 
+    event RevTransferredToUser(uint txn_id);
+    event RevCustodianChallengeAccepted(uint txn_id);
 
     function set_custodian(address addr) public {
         require(msg.sender == m_owner);
@@ -34,57 +55,84 @@ contract UserRSKContract is mortal {
 
     function fwd_create_transaction(uint txn_id, bytes32 custodian_pwd_hash, 
                                 uint timeout_interval, uint sbtc_amount) public { /* By user */
-        require(m_txns[txn_id].txn_id != txn_id, "Transaction already exists");
-       
-        m_txns[txn_id] = ForwardTxn(msg.sender, txn_id, custodian_pwd_hash, timeout_interval,
-                                    block.number, sbtc_amount, TxnStates.CREATED);
-        
-        emit UserTransactionCreated(txn_id, msg.sender, m_custodian_rsk);
+        require(m_fwd_txns[txn_id].txn_id != txn_id, "Transaction already exists");
+        m_fwd_txns[txn_id] = ForwardTxn(txn_id, msg.sender, custodian_pwd_hash, timeout_interval,
+                                    block.number, sbtc_amount, FwdTxnStates.CREATED);
+        emit FwdUserTransactionCreated(txn_id, msg.sender, m_custodian_rsk);
     }
 
     function fwd_transfer_to_contract(uint txn_id) public payable { /* To be called by user */
-        ForwardTxn memory txn = m_txns[txn_id]; /* Convenience. TODO: Check if this is reference or a copy */
+        ForwardTxn memory txn = m_fwd_txns[txn_id]; /* Convenience. TODO: Check if this is reference or a copy */
         require(msg.sender == txn.user_rsk);
-        require(txn.txn_id == txn_id, "Transaction does not exist"); 
+        require(txn.state == FwdTxnStates.CREATED);
         require(msg.value == txn.sbtc_amount, "Sent SBTC amount does not match"); 
+ 
+        txn.state = FwdTxnStates.TRANSFERRED;
 
-        emit UserTransferred(txn_id);
-    }
-
-    function fwd_request_refund(uint txn_id) public { /* Called by user */
-        ForwardTxn memory txn = m_txns[txn_id]; 
-        require(msg.sender == txn.user_rsk, "Only user can call this"); 
-        require(txn.state == TxnStates.CREATED, "Transaction not in CREATED state"); 
-        require(block.number > (txn.creation_block + txn.timeout_interval));
-
-        txn.user_rsk.transfer(txn.sbtc_amount);
-        txn.state = TxnStates.REFUNDED;
-
-        emit RefundedToUser(txn_id);
+        emit FwdUserTransferred(txn_id);
     }
 
     /* This function main job is to send password string to user */ 
-    function fwd_send_pwd(uint txn_id, string pwd_str) public { /* Called by custodian */
-        ForwardTxn memory txn = m_txns[txn_id]; 
+    function fwd_execute(uint txn_id, string pwd_str) public { /* Called by custodian */
+        ForwardTxn memory txn = m_fwd_txns[txn_id]; 
         require(msg.sender == m_custodian_rsk, "Only custodian can call this"); 
-        require(txn.state == TxnStates.CREATED, "Transaction not in CREATED state");
+        require(txn.state == FwdTxnStates.TRANSFERRED, "Transaction not in TRANSFERRED state");
         require(block.number <= (txn.creation_block + txn.timeout_interval));
         require(txn.custodian_pwd_hash == keccak256(pwd_str), "Hash does not match");
+  
+        m_custodian_rsk.transfer(txn.sbtc_amount);
+        txn.state = FwdTxnStates.EXECUTED;
 
-        txn.state = TxnStates.EXECUTED;
-
-        emit CustodianExecutionSuccess(txn_id, pwd_str);
+        emit FwdCustodianExecutionSuccess(txn_id, pwd_str);
     }
 
-    function rev_execute(address to, uint sbtc_amount) public { /* By custodian */
+    /* Refund in case no action by Custodian */ 
+    function fwd_request_refund(uint txn_id) public { /* Called by user */
+        ForwardTxn memory txn = m_fwd_txns[txn_id]; 
+        require(msg.sender == txn.user_rsk, "Only user can call this"); 
+        require(txn.state == FwdTxnStates.TRANSFERRED, "Transaction not in TRANSFERRED state"); 
+        require(block.number > (txn.creation_block + txn.timeout_interval));
+
+        txn.user_rsk.transfer(txn.sbtc_amount);
+        txn.state = FwdTxnStates.REFUNDED;
+
+        emit FwdRefundedToUser(txn_id);
+    }
+
+    function rev_deposit(uint txn_id, address user_rsk, address dest_rsk_addr, uint sbtc_amount,
+                         bytes32 ack_hash) payable public {  /* Called by custodian */
+        /* The custodian should pay enough so that lock amount is covered */
         require(msg.sender == m_custodian_rsk, "Only custodian can call this"); 
-        /* TODO: We need to check if the amount is going to correct user */
-        to.transfer(sbtc_amount);
+        require(address(this).balance.sub(m_locked_sbtc) >= sbtc_amount); 
+        m_locked_sbtc += sbtc_amount;
+
+        m_rev_txns[txn_id] = ReverseTxn(txn_id, user_rsk, dest_rsk_addr, ack_hash, sbtc_amount, block.number, RevTxnStates.DEPOSITED); 
+         
+        emit RevCustodianDeposited(txn_id, ack_hash); 
     }
 
-    function rev_force_execute() public { /* By user if custodian does not call rev_excecute()  */
-        if txn 
+    function rev_transfer(uint txn_id, bytes ack_str) public { /* Called by user */
+        ReverseTxn memory txn = m_rev_txns[txn_id];
+        require(msg.sender == txn.user_rsk); 
+        require(txn.ack_hash == keccak256(ack_str)); 
+       
+        txn.dest_rsk_addr.transfer(txn.sbtc_amount);
+        txn.state = RevTxnStates.TRANSFERRED; 
+        
+        emit RevTransferredToUser(txn_id);
+    } 
 
+    function rev_challenge(uint txn_id) public { /* Called by custodian if user hasn't acted for a while */
+        ReverseTxn memory txn = m_rev_txns[txn_id];
+        require(msg.sender == m_custodian_rsk); 
+        require(block.number > txn.creation_block + m_sbtc_lock_interval); 
+        require(txn.state == RevTxnStates.DEPOSITED);
+         
+        m_custodian_rsk.transfer(txn.sbtc_amount); /* TODO: Check if transfer() fails, the whole transaction is reverted */
+        m_locked_sbtc -= txn.sbtc_amount;
+        txn.state = RevTxnStates.CUSTODIAN_CHALLENGE_ACCEPTED;
+
+        emit RevCustodianChallengeAccepted(txn_id);
     }
 
 }
