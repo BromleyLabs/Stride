6ab125e5a3d443b5442ea668ec6f3ea52c3b1c2f
Bon Filey <puneet@outdu.com>
2018-05-21 09:26:56 +0530

    Updated.

---
+++ app/common/deploy.py
@@ -0,0 +1,24 @@
+from web3 import Web3
+from hexbytes import HexBytes
+import sys
+from common.utils import * 
+from common import config
+
+def main():
+    if len(sys.argv) != 3:
+        print('Usage: python deploy.py <rsk | eth> <contract_name>')
+        exit(0)
+
+    logger = init_logger('DEPLOY', '/tmp/stride.log')
+    if sys.argv[1] == 'rsk':
+        chain = W3Utils(config.rsk, logger)
+    elif sys.argv[1] == 'eth':
+        chain = W3Utils(config.eth, logger)
+    else:
+        printf('Incorrect argument')
+
+    tx_receipt = chain.deploy(sys.argv[2])
+    logger.info('Contact address = %s' % tx_receipt['contractAddress'])
+
+if __name__== '__main__':
+    main()
