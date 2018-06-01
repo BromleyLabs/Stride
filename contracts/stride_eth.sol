d886bb873e763842484d3e3b5007e2d2e982e76d
Bon Filey <puneet@outdu.com>
2018-06-01 15:45:16 +0530

    P1v3 - WIP.

--- contracts/stride_eth.sol
+++ contracts/stride_eth.sol
@@ -5,194 +5,55 @@ pragma solidity ^0.4.23;
 import "erc20.sol";
 import "ebtc_token.sol";
 import "mortal.sol";
+import "github.com/oraclize/ethereum-api/oraclizeAPI_0.5.sol";
+import "JsmnSolLib.sol";
 
-contract StrideEthContract is mortal {
-    using SafeMath for uint;
+contract StrideEthContract is mortal,usingOraclize {
 
-    enum FwdTxnStates {UNINITIALIZED, DEPOSITED, ISSUED, CHALLENGED}
-    enum RevTxnStates {UNINITIALIZED, DEPOSITED, HASH_ADDED, SECURITY_RECOVERED, CHALLENGED}
+    mapping (bytes32 => bool) public issued;
+    string public rskOracleURL = "http://ropsten.bromleylabs.io";
+    address public rskContractAddr = 0x0; 
+    mapping public (bytes32 => bool) validIds;
 
-    struct ForwardTxn { /* SBTC -> EBTC Transaction */
-        uint txn_id; 
-        address user_eth; 
-        bytes32 custodian_pwd_hash; /* Custodian password hash */
-        uint timeout_interval; /* Blocks */
-        uint creation_block; /* When this transaction was created */ 
-        uint ebtc_amount; 
-        uint collateral_eth; /* Calculated */
-        FwdTxnStates state;
-    }  
+    event LogNewOraclizeQuery(string description);
 
-    struct ReverseTxn { /* EBTC -> SBTC transaction */
-        uint txn_id;
-        address user_eth;
-        address dst_rsk; /* Destination address on RSK */ 
-        uint ebtc_amount;  
-        uint creation_block;
-        bytes32 ack_hash;
-        uint security_eth; 
-        RevTxnStates state; 
-    }
-
-    mapping (uint => ForwardTxn) public m_fwd_txns; 
-    mapping (uint => ReverseTxn) public m_rev_txns; 
-
-    address public m_custodian_eth;
-    address public m_ebtc_token_addr; /* Set by method below */ 
-    uint public m_eth_ebtc_ratio_numerator = 15;  /* For collateral */
-    uint public m_eth_ebtc_ratio_denominator = 1;
-    uint public m_ether_lock_interval = 100; /* In blocks */
-    uint public m_locked_eth = 0;
-
-    event FwdCustodianDeposited(uint txn_id);
-    event FwdEBTCIssued(uint txn_id);
-    event FwdCustodianChallengeAccepted(uint txn_id);
-
-    event RevCollateralDeposited(uint txn_id);
-    event RevRedemptionInitiated(uint txn_id, address dest_rsk_addr);
-    event RevHashAdded(uint txn_id, bytes32 ack_hash);
-    event RevCustodianSecurityRecovered(uint txn_id, string ack_str);
-    event RevUserChallengeAccepted(uint txn_id);
-    event RevCustodianChallengeAccepted(uint txn_id);
-   
-    /* Contract initialization functions called by Owner */
-    function set_custodian(address addr) public {
-        require(msg.sender == m_owner);
-        m_custodian_eth = addr;
-    }
-
-    function set_ebtc_token_address(address addr) public {
+    function setRSKContractAddr(address addr) public {
         require(msg.sender == m_owner, "Only owner can set this");
-        m_ebtc_token_addr = addr; 
+        rskContractAddr = addr;
     } 
 
-    function set_eth_ebtc_ratio(uint numerator, uint denominator) public {
+    function setRSKOracleURL(string url) public {
         require(msg.sender == m_owner, "Only owner can set this");
-        m_eth_ebtc_ratio_numerator = numerator;
-        m_eth_ebtc_ratio_denominator = denominator;
-    }
-
-    function set_lock_interval(uint nblocks) public {
-        require(msg.sender == m_owner, "Only owner can set this");
-        m_ether_lock_interval = nblocks;
-    }
-
-    /* Called by custodian. Send collateral Eth to contract */
-    function fwd_deposit(uint txn_id, address user_eth, bytes32 custodian_pwd_hash, 
-                         uint timeout_interval, uint ebtc_amount) public payable {
-        require(txn_id > 0);
-        require(m_fwd_txns[txn_id].txn_id != txn_id, "Transaction already exists");
-        require(msg.sender == m_custodian_eth);
-        uint collateral_eth = (ebtc_amount.mul(m_eth_ebtc_ratio_numerator)).div(m_eth_ebtc_ratio_denominator); 
-        require(msg.value == collateral_eth); 
-
-        m_fwd_txns[txn_id] = ForwardTxn(txn_id, user_eth, custodian_pwd_hash, timeout_interval,
-                                    block.number, ebtc_amount, collateral_eth, FwdTxnStates.DEPOSITED);
-        
-        emit FwdCustodianDeposited(txn_id); 
-    }
-
-    /* Called by user.  Issue EBTCs to user */
-    function fwd_issue(uint txn_id, string pwd_str) public { 
-        ForwardTxn storage txn = m_fwd_txns[txn_id]; 
-        require(msg.sender == txn.user_eth, "Only user can call this"); 
-        require(txn.state == FwdTxnStates.DEPOSITED, "Transaction not in DEPOSITED state");
-        require(block.number <= (txn.creation_block + txn.timeout_interval));
-        require(txn.custodian_pwd_hash == keccak256(pwd_str), "Hash does not match");
-
-        require(EBTCToken(m_ebtc_token_addr).issueFreshTokens(txn.user_eth, txn.ebtc_amount));
-        txn.state = FwdTxnStates.ISSUED;
-
-        emit FwdEBTCIssued(txn_id);
-    }
-
-    /* Called by custodian when no user action */
-    function fwd_no_user_action_challenge(uint txn_id) public {
-        ForwardTxn storage txn = m_fwd_txns[txn_id]; 
-        require(msg.sender == m_custodian_eth, "Only custodian can call this"); 
-        require(txn.state == FwdTxnStates.DEPOSITED, "Transaction not in DEPOSITED state"); 
-        require(block.number > (txn.creation_block + txn.timeout_interval));
-
-        m_custodian_eth.transfer(txn.collateral_eth);
-        txn.state = FwdTxnStates.CHALLENGED;
-
-        emit FwdCustodianChallengeAccepted(txn_id);
-    }
-
-    /* Called by user for EBTC->SBTC conversion */
-    function rev_request_redemption(uint txn_id, address dest_rsk_addr, uint ebtc_amount) public { 
-        /* User creates a unique redemption id */
-        require(txn_id > 0);
-        require(m_rev_txns[txn_id].txn_id != txn_id, "Transaction already exists");
-        uint security_eth = (ebtc_amount.mul(m_eth_ebtc_ratio_numerator)).div(m_eth_ebtc_ratio_denominator); 
-        require(address(this).balance.sub(m_locked_eth) >= security_eth); 
-
-        /* Assuming this contract has been given approval to move funds */ 
-        require(EBTCToken(m_ebtc_token_addr).transferFrom(msg.sender, this, ebtc_amount));
-        require(EBTCToken(m_ebtc_token_addr).burnTokens(ebtc_amount));
-        
-        m_locked_eth += security_eth;  /* Lock Ether */
-        m_rev_txns[txn_id] = ReverseTxn(txn_id, msg.sender, dest_rsk_addr, ebtc_amount, 
-                                        block.number, 0, security_eth, RevTxnStates.DEPOSITED); 
-
-        emit RevRedemptionInitiated(txn_id, dest_rsk_addr);
+        rskOracleURL = url; 
     } 
 
-   /* Called by user */
-   function rev_add_hash(uint txn_id, bytes32 ack_hash) public { 
-       ReverseTxn storage txn = m_rev_txns[txn_id];
-       require(msg.sender == txn.user_eth);
-       require(txn.state == RevTxnStates.DEPOSITED);
-       txn.ack_hash = ack_hash;
-       txn.state = RevTxnStates.HASH_ADDED;
-       
-       emit RevHashAdded(txn_id, ack_hash);
-   } 
-    
-   /* Called by custodian after RSK contract receives expected SBTCs */ 
-   function rev_recover_security_deposit(uint txn_id, string ack_str) public {
-       ReverseTxn storage txn = m_rev_txns[txn_id];
-       require(msg.sender == m_custodian_eth);
-       require(txn.state == RevTxnStates.HASH_ADDED);
-       require(txn.ack_hash == keccak256(ack_str)); 
+    function issueEBTC(string jsonHashRequest) public { 
+        /* Of form: '{"jsonrpc" : "2.0", "id" : 0, 
+           "method" : "eth_getTransactionByHash", 
+           "params" : 
+        ["0x3a232165aa0ae7ae7ea94a3162ee6a8a829390f167ea83e150af204df8624239"]}'
+        */
 
-       m_custodian_eth.transfer(txn.security_eth); 
-       m_locked_eth -= txn.security_eth; /* Unlock Ether */
-       txn.state = RevTxnStates.SECURITY_RECOVERED; 
+        /* There should be enough balance for all Oraclize queries */
+        require(oraclize_getPrice("URL") * 2 > this.balance, 
+                "Oraclize query not send"); 
 
-       emit RevCustodianSecurityRecovered(txn_id, ack_str);
-   } 
+        /* Obtain transaction info from RSK */ 
+        bytes32 queryId; 
+        queryId = oraclize_query(
+            "URL", 
+            "json(http://ropsten.bromleylabs.io).result",
+            jsonHashRequest
+        ) 
+        validIds[queryId] = true;
 
-   /* Called by custodian if no action by user */
-   function rev_no_user_action_challenge(uint txn_id) public {
-       ReverseTxn storage txn = m_rev_txns[txn_id];
-       require(msg.sender == m_custodian_eth);
-       require(txn.state == RevTxnStates.DEPOSITED);
-       require(block.number > txn.creation_block + m_ether_lock_interval); 
-
-       m_custodian_eth.transfer(txn.security_eth); 
-       m_locked_eth -= txn.security_eth; /* Unlock Ether */
-
-       emit RevCustodianChallengeAccepted(txn_id);
-   }
-
-   /* Called by user if no action has been taken by custodian */
-   function rev_no_custodian_action_challenge(uint txn_id) public { 
-       ReverseTxn storage txn = m_rev_txns[txn_id];
-       require(msg.sender == txn.user_eth);
-       require(txn.state == RevTxnStates.DEPOSITED);
-       require(block.number > txn.creation_block + m_ether_lock_interval); 
-       
-       txn.user_eth.transfer(txn.security_eth); /* penalty */
-       txn.state = RevTxnStates.CHALLENGED;
-
-       emit RevUserChallengeAccepted(txn_id);
-   }
-
-   /* Dummy function to transfer Ethers to this contract */
-   function consume_eth() public payable {
-   }
+        /* Obtain latest block number from RSK */ 
+        queryId = oraclize_query(
+            "URL", 
+            "json(http://ropsten.bromleylabs.io).result",
+            '{"jsonrpc" : "2.0", "id" : 0, "method" : "eth_blockNumber", "params" : []}')
+        validIds[queryId] = true;
+    }
 
 }
-
 
--- contracts/stride_rsk.sol
+++ contracts/stride_rsk.sol
@@ -2,145 +2,15 @@ pragma solidity ^0.4.23;
 
 /* Contract on RSK for Stride transactions */ 
 
-import "erc20.sol";
 import "mortal.sol";
 import "safe_math.sol";
 
 contract StrideRSKContract is mortal {
     using SafeMath for uint;
 
-    enum FwdTxnStates {UNINITIALIZED, DEPOSITED, TRANSFERRED, CHALLENGED}
-    enum RevTxnStates {UNINITIALIZED, DEPOSITED, TRANSFERRED, CHALLENGED}  
+    event UserDeposited(address userRSK, uint sbtcAmount);
 
-    struct ForwardTxn {  /* SBTC -> EBTC Transaction */
-        uint txn_id; 
-        address user_rsk; /* RSK address */
-        bytes32 custodian_pwd_hash; /* Custodian password hash */
-        uint timeout_interval; /* Blocks. Arbitary */ 
-        uint creation_block; 
-        uint sbtc_amount;
-        FwdTxnStates state;
-    } 
-
-    struct ReverseTxn {  /* EBTC -> SBTC Transaction */
-        uint txn_id; 
-        address user_rsk;
-        address dest_rsk_addr;
-        bytes32 ack_hash; 
-        uint sbtc_amount;
-        uint creation_block;
-        RevTxnStates state;
+    function depositSBTC(address eth_dest_addr) public payable {
+        emit UserDeposited(msg.sender, msg.value); 
     }
-
-    mapping (uint => ForwardTxn) public m_fwd_txns;  /* TODO: Efficient and cheaper storage and purge strategy of transactions */ 
-    mapping (uint => ReverseTxn) public m_rev_txns; 
-
-    address public m_custodian_rsk;   
-    uint public m_locked_sbtc = 0;
-    uint public m_sbtc_lock_interval = 100;  /* In blocks. */
-
-    event FwdUserDeposited(uint txn_id);
-    event FwdTransferredToCustodian(uint txn_id, string pwd_str); 
-    event FwdUserChallengeAccepted(uint txn_id);
-
-    event RevCustodianDeposited(uint txn_id, bytes32 ack_hash); 
-    event RevTransferredToUser(uint txn_id);
-    event RevCustodianChallengeAccepted(uint txn_id);
-
-    /* Contract initialization functions called by Owner */
-    function set_custodian(address addr) public {
-        require(msg.sender == m_owner);
-        m_custodian_rsk = addr;
-    }
-
-    function set_lock_interval(uint nblocks) public {
-        require(msg.sender == m_owner, "Only owner can set this");
-        m_sbtc_lock_interval = nblocks;
-    }
-
-    /* Called by user. Transfer SBTC to contract */
-    function fwd_deposit(uint txn_id, bytes32 custodian_pwd_hash, 
-                                uint timeout_interval) public payable {
-        require(txn_id > 0);
-        require(m_fwd_txns[txn_id].txn_id != txn_id, "Transaction already exists");
-        require(msg.value > 0, "SBTC cannot be 0"); /* NOTE: custodian may want to check if this amount is as per agreed while hash off-chain transaction */
-
-        m_fwd_txns[txn_id] = ForwardTxn(txn_id, msg.sender, custodian_pwd_hash, timeout_interval,
-                                    block.number, msg.value, FwdTxnStates.DEPOSITED);
-        emit FwdUserDeposited(txn_id);
-    }
-
-    /* Called by custodian. Send password string to user and transfer SBTC to custodian */
-    function fwd_transfer(uint txn_id, string pwd_str) public { 
-        ForwardTxn storage txn = m_fwd_txns[txn_id]; 
-        require(msg.sender == m_custodian_rsk, "Only custodian can call this"); 
-        require(txn.state == FwdTxnStates.DEPOSITED, "Transaction not in DEPOSITED state");
-        require(block.number <= (txn.creation_block + txn.timeout_interval));
-        require(txn.custodian_pwd_hash == keccak256(pwd_str), "Hash does not match");
-  
-        m_custodian_rsk.transfer(txn.sbtc_amount);
-        txn.state = FwdTxnStates.TRANSFERRED;
-
-        emit FwdTransferredToCustodian(txn_id, pwd_str);
-    }
-
-    /* Called by user. Refund in case no action by Custodian */ 
-    function fwd_no_custodian_action_challenge(uint txn_id) public {
-        ForwardTxn storage txn = m_fwd_txns[txn_id]; 
-        require(msg.sender == txn.user_rsk, "Only user can call this"); 
-        require(txn.state == FwdTxnStates.DEPOSITED, "Transaction not in DEPOSITED state"); 
-        require(block.number > (txn.creation_block + txn.timeout_interval));
-
-        txn.user_rsk.transfer(txn.sbtc_amount);
-        txn.state = FwdTxnStates.CHALLENGED;
-
-        emit FwdUserChallengeAccepted(txn_id);
-    }
-
-    /* Called by custodian. Send enough SBTCs to contract to pay to user. */
-    function rev_deposit(uint txn_id, address user_rsk, address dest_rsk_addr, uint sbtc_amount,
-                         bytes32 ack_hash) payable public {  
-        require(txn_id > 0);
-        require(m_rev_txns[txn_id].txn_id != txn_id, "Transaction already exists");
-        /* The custodian should pay enough so that lock amount is covered */
-        require(msg.sender == m_custodian_rsk, "Only custodian can call this"); 
-        /* Ensure custodian has transferred enough for locking SBTCs */
-        require((address(this).balance - m_locked_sbtc + msg.value) >= sbtc_amount); 
-        m_locked_sbtc += sbtc_amount;
-
-        m_rev_txns[txn_id] = ReverseTxn(txn_id, user_rsk, dest_rsk_addr, ack_hash, sbtc_amount, 
-                                        block.number, RevTxnStates.DEPOSITED); 
-         
-        emit RevCustodianDeposited(txn_id, ack_hash); 
-    }
-
-    /* Called by user.  Transfer SBTCs to destination address */
-    function rev_transfer(uint txn_id, string ack_str) public { 
-        ReverseTxn storage txn = m_rev_txns[txn_id];
-        require(msg.sender == txn.user_rsk, "Only RSK user can call this"); 
-        require(txn.ack_hash == keccak256(ack_str), "Hash does not match"); 
-        require(block.number <= txn.creation_block + m_sbtc_lock_interval, "Timed out");  /* Within certain time period */
-       
-        txn.dest_rsk_addr.transfer(txn.sbtc_amount);
-        txn.state = RevTxnStates.TRANSFERRED; 
-        
-        emit RevTransferredToUser(txn_id);
-    } 
-
-    /* Called by custodian. Recover custodian's SBTCs if user hasn't acted for a while. */
-    function rev_challenge(uint txn_id) public { 
-        ReverseTxn storage txn = m_rev_txns[txn_id];
-        require(msg.sender == m_custodian_rsk); 
-        require(block.number > txn.creation_block + m_sbtc_lock_interval); 
-        require(txn.state == RevTxnStates.DEPOSITED);
-         
-        m_custodian_rsk.transfer(txn.sbtc_amount); 
-        m_locked_sbtc -= txn.sbtc_amount;
-        txn.state = RevTxnStates.CHALLENGED;
-
-        emit RevCustodianChallengeAccepted(txn_id);
-    }
-
-}
-
-
+} 
