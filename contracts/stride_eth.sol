d1ceacd8ee1c5441ded4cd14026d5a15f5b32e8c
Bon Filey <puneet@outdu.com>
2018-06-03 05:32:55 +0530

    WIP.

--- contracts/stride_eth.sol
+++ contracts/stride_eth.sol
@@ -10,10 +10,19 @@ import "JsmnSolLib.sol";
 
 contract StrideEthContract is mortal,usingOraclize {
 
-    mapping (bytes32 => bool) public issued;
+    struct FwdTxn {
+        bytes32 txnHash;
+        bytes32 txnQueryId; 
+        bytes32 blockQueryId;
+        uint state;
+        bool issued;
+    }
+
     string public rskOracleURL = "http://ropsten.bromleylabs.io";
     address public rskContractAddr = 0x0; 
-    mapping public (bytes32 => bool) validIds;
+    mapping public (bytes32 => FwdTxn) fwdTxns; /* queryId => FwdTxn */
+    bytes32 txnQueryId;
+    bytes32 blockQueryId;
 
     event LogNewOraclizeQuery(string description);
 
@@ -27,32 +36,48 @@ contract StrideEthContract is mortal,usingOraclize {
         rskOracleURL = url; 
     } 
 
-    function issueEBTC(string jsonHashRequest) public { 
-        /* Of form: '{"jsonrpc" : "2.0", "id" : 0, 
-           "method" : "eth_getTransactionByHash", 
-           "params" : 
-        ["0x3a232165aa0ae7ae7ea94a3162ee6a8a829390f167ea83e150af204df8624239"]}'
-        */
+    function __callback(bytes32 queryId, string result) {
+        require(msg.sender != oraclize_cbAddress());
+        if (queryId == fwdTxns[queryId].txnQueryId) {
+            /* Parse txn json */
+        }
 
+        if (queryId == fwdTxns[queryId].blockQueryId) { 
+            /* Parse block number */
+        }
+
+        delete validIds[queryId]; 
+    }
+
+    /* Called by user. 
+       jsonHashRequest: '{"jsonrpc" : "2.0", "id" : 0, "method" : 
+       "eth_getTransactionByHash", "params" : ["0x<transaction hash>"]}'
+    */
+    function issueEBTC(bytes32 txnHash, string jsonHashRequest) public { 
         /* There should be enough balance for all Oraclize queries */
-        require(oraclize_getPrice("URL") * 2 > this.balance, 
-                "Oraclize query not send"); 
+        require(
+            oraclize_getPrice("URL") * 2 > this.balance, 
+            "Oraclize query not send"
+        ); 
 
         /* Obtain transaction info from RSK */ 
-        bytes32 queryId; 
-        queryId = oraclize_query(
+        txnQueryId = oraclize_query(
             "URL", 
             "json(http://ropsten.bromleylabs.io).result",
             jsonHashRequest
-        ) 
-        validIds[queryId] = true;
+       ); 
 
         /* Obtain latest block number from RSK */ 
-        queryId = oraclize_query(
+        blockQueryId = oraclize_query(
             "URL", 
             "json(http://ropsten.bromleylabs.io).result",
-            '{"jsonrpc" : "2.0", "id" : 0, "method" : "eth_blockNumber", "params" : []}')
-        validIds[queryId] = true;
+            '{"jsonrpc" : "2.0", "id" : 0, "method" : "eth_blockNumber",\
+                "params" : []}'
+        );
+
+        FwdTxn txn = FwdTxn(txnHash, 0, false);
+        fwdTxns[txnQueryId] = txn; 
+        fwdTxns[blockQueryId] = txn; 
     }
 
 }
