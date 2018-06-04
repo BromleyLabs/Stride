9ab631c76987b3115a7a3b8656e07b8a7341678a
Bon Filey <puneet@outdu.com>
2018-06-04 21:32:28 +0530

    Reverse transaction coded.

--- contracts/stride_eth.sol
+++ contracts/stride_eth.sol
@@ -5,9 +5,13 @@ pragma solidity ^0.4.23;
 import "erc20.sol";
 import "ebtc_token.sol";
 import "mortal.sol";
+import "utils.sol";
 import "github.com/oraclize/ethereum-api/oraclizeAPI_0.5.sol";
 
+
 contract StrideEthContract is mortal,usingOraclize {
+    using SafeMath for uint;
+    using StrideUtils for bytes;
 
     struct FwdTxn {
         bytes32 txn_hash;
@@ -19,6 +23,8 @@ contract StrideEthContract is mortal,usingOraclize {
     mapping(bytes32 => FwdTxn) m_fwd_txns;
     mapping(bytes32 => bytes32) m_query_map;
     uint public m_min_confirmations = 30;
+
+    event UserRedeemed(address user_eth, uint ebtc_amount);
 
     function setRSKContractAddr(address addr) public {
         require(msg.sender == m_owner, "Only owner can set this");
@@ -35,22 +41,6 @@ contract StrideEthContract is mortal,usingOraclize {
         m_ebtc_token_addr = addr;
     }
 
-    /* Utility function to extract bytes from a byte array given an offset 
-       and returns as bytes32 */
-    function getBytes32(bytes b, uint offset) private pure returns (bytes32) {
-        bytes32 out;
-        for (uint i = 0; i < 32; i++) 
-            out |= bytes32(b[offset + i] & 0xFF) >> (i * 8); 
-        return out;
-    }
-
-    function getBytes20(bytes b, uint offset) private pure returns (bytes20) {
-        bytes20 out;
-        for (uint i = 0; i < 20; i++) 
-            out |= bytes20(b[offset + i] & 0xFF) >> (i * 8); 
-        return out;
-    }
-
     /* Called by Oraclize. 
        result bytes format: current_block(32), txn_block(32), 
                             contract_address(20), dest_addr(20), sbtc(32)
@@ -63,25 +53,26 @@ contract StrideEthContract is mortal,usingOraclize {
         require(!txn.issued, "Transaction already issued");
 
         bytes memory b = bytes(result);
+        require(b.length > 0, "Transaction incorrect");
 
         uint offset = 0; 
-        uint txn_block = uint(getBytes32(b, offset));
+        uint txn_block = uint(b.getBytes32(offset));
 
         offset += 32;  
-        uint current_block = uint(getBytes32(b, offset));
+        uint current_block = uint(b.getBytes32(offset));
 
-        require(current_block - txn_block > m_min_confirmations, 
+        require(current_block.sub(txn_block) > m_min_confirmations, 
                 "Confirmations not enough");
 
         offset += 32;  
-        address to_addr = address(getBytes20(b, offset));
+        address to_addr = address(b.getBytes20(offset));
         require(to_addr == m_rsk_addr, "To address does not match");
 
         offset += 20;  
-        address dest_addr = address(getBytes20(b, offset));
+        address dest_addr = address(b.getBytes20(offset));
 
         offset += 20;  
-        uint ebtc_amount = uint(getBytes32(b, offset)); /* == sbtc */
+        uint ebtc_amount = uint(b.getBytes32(offset)); /* == sbtc */
    
         require(EBTCToken(m_ebtc_token_addr).issueFreshTokens(dest_addr,
                                                               ebtc_amount));
@@ -101,15 +92,23 @@ contract StrideEthContract is mortal,usingOraclize {
 
         /* Obtain transaction info from RSK */ 
         bytes32 query_id;
-        query_id = oraclize_query("URL", 
-                                  "json(http://ropsten.bromleylabs.io).result",
-                                  json_request); 
+        query_id = oraclize_query(
+                      "URL", 
+                      "json(http://api.bromleylabs.io/rsk/testnet).result",
+                      json_request); 
 
        if(m_fwd_txns[txn_hash].txn_hash != txn_hash) /* May already exist */ 
            m_fwd_txns[txn_hash] = FwdTxn(txn_hash, false); 
 
        m_query_map[query_id] = txn_hash;
     }
+
+   function redeem(address rsk_dest_addr, uint ebtc_amount) {
+       /* Assumed user has approved EBTC transfer by this contract */ 
+       require(EBTCToken(m_ebtc_token_addr).transferFrom(msg.sender, 0x0, 
+                                                         ebtc_amount));
+       emit UserRedeemed(msg.sender, ebtc_amount);
+   }
 
 }
 
--- contracts/stride_rsk.sol
+++ contracts/stride_rsk.sol
@@ -2,15 +2,100 @@ pragma solidity ^0.4.23;
 
 /* Contract on RSK for Stride transactions */ 
 
-import "mortal.sol";
 import "safe_math.sol";
+import "mortal.sol";
+import "utils.sol";
+import "github.com/oraclize/ethereum-api/oraclizeAPI_0.5.sol";
 
-contract StrideRSKContract is mortal {
+contract StrideRSKContract is mortal, usingOraclize {
     using SafeMath for uint;
+    using StrideUtils for bytes;
+
+    struct RevTxn {
+        bytes32 txn_hash;
+        bool redeemed;
+    }
+
+    address public m_eth_addr; /* Set by method below */
+    mapping(bytes32 => RevTxn) m_rev_txns;
+    mapping(bytes32 => bytes32) m_query_map;
+    uint public m_min_confirmations = 30;
 
     event UserDeposited(address userRSK, uint sbtcAmount);
 
     function depositSBTC(address eth_dest_addr) public payable {
         emit UserDeposited(msg.sender, msg.value); 
+    }
+
+    function setEthContractAddr(address addr) public {
+        require(msg.sender == m_owner, "Only owner can set this");
+        m_eth_addr = addr;
+    } 
+
+    function setMinConfirmations(uint n) public {
+        require(msg.sender == m_owner, "Only owner can set this");
+        m_min_confirmations = n;
+    }
+
+    /* Called by Oraclize. 
+       result bytes format: current_block(32), txn_block(32), 
+                            contract_address(20), dest_addr(20), sbtc(32)
+    */  
+    function __callback(bytes32 query_id, string result) public {
+        require(msg.sender != oraclize_cbAddress());
+
+        bytes32 txn_hash = m_query_map[query_id];
+        RevTxn storage txn = m_rev_txns[txn_hash];
+        require(!txn.redeemed, "Transaction already issued");
+
+        bytes memory b = bytes(result);
+        require(b.length > 0, "Transaction incorrect");
+
+        uint offset = 0; 
+        uint txn_block = uint(b.getBytes32(offset));
+
+        offset += 32;  
+        uint current_block = uint(b.getBytes32(offset));
+
+        require(current_block - txn_block > m_min_confirmations, 
+                "Confirmations not enough");
+
+        offset += 32;  
+        address to_addr = address(b.getBytes20(offset));
+        require(to_addr == m_eth_addr, "To address does not match");
+
+        offset += 20;  
+        address dest_addr = address(b.getBytes20(offset));
+
+        offset += 20;  
+        uint sbtc_amount = uint(b.getBytes32(offset)); /* == ebtc */
+   
+        dest_addr.transfer(sbtc_amount);
+       
+        txn.redeemed = true;
+
+        delete m_query_map[query_id];
+    }
+
+    /* Called by user. 
+       jsonHashRequest: '{"jsonrpc" : "2.0", "id" : 0, "method" : 
+       "eth_getTransactionByHash", "params" : ["0x<transaction hash>"]}'
+    */
+    function redeem(bytes32 txn_hash, string json_request) public { 
+        /* There should be enough balance for all Oraclize queries */
+        require(oraclize_getPrice("URL")  > address(this).balance, 
+                                  "Oraclize query not send"); 
+
+        /* Obtain transaction info from RSK */ 
+        bytes32 query_id;
+        query_id = oraclize_query(
+                      "URL", 
+                      "json(http://api.bromleylabs.io/ethereum/ropsten).result",
+                      json_request); 
+
+       if(m_rev_txns[txn_hash].txn_hash != txn_hash) /* May already exist */ 
+           m_rev_txns[txn_hash] = RevTxn(txn_hash, false); 
+
+       m_query_map[query_id] = txn_hash;
     }
 } 
