8cc89964502c75419872d38b96da778d7c3d51e3
Bon Filey <puneet@outdu.com>
2018-06-04 16:56:29 +0530

    Compilation errors removed.

--- contracts/stride_eth.sol
+++ contracts/stride_eth.sol
@@ -11,8 +11,6 @@ contract StrideEthContract is mortal,usingOraclize {
 
     struct FwdTxn {
         bytes32 txn_hash;
-        uint blocknumber_ok; /* enough confirmations */
-        uint txn_ok; 
         bool issued;
     }
 
@@ -22,7 +20,7 @@ contract StrideEthContract is mortal,usingOraclize {
     mapping(bytes32 => bytes32) m_query_map;
     uint public m_min_confirmations = 30;
 
-    function setRSKContractAddr(address addr, string addr_str) public {
+    function setRSKContractAddr(address addr) public {
         require(msg.sender == m_owner, "Only owner can set this");
         m_rsk_addr = addr;
     } 
@@ -32,7 +30,7 @@ contract StrideEthContract is mortal,usingOraclize {
         m_min_confirmations = n;
     }
 
-    function setEBTCTokenAddress(address addr) {
+    function setEBTCTokenAddress(address addr) public {
         require(msg.sender == m_owner, "Only owner can set this");
         m_ebtc_token_addr = addr;
     }
@@ -57,14 +55,14 @@ contract StrideEthContract is mortal,usingOraclize {
        current_block(32), txn_block(32), contract_address(20), dest_addr(20), 
        sbtc(32)
     */  
-    function __callback(bytes32 query_id, string result) {
+    function __callback(bytes32 query_id, string result) public {
         require(msg.sender != oraclize_cbAddress());
 
         bytes32 txn_hash = m_query_map[query_id];
         FwdTxn storage txn = m_fwd_txns[txn_hash];
         require(!txn.issued, "Transaction already issued");
 
-        bytes b = bytes(result);
+        bytes memory b = bytes(result);
 
         uint offset = 0; 
         uint txn_block = uint(getBytes32(b, offset));
@@ -77,15 +75,19 @@ contract StrideEthContract is mortal,usingOraclize {
 
         offset += 32;  
         address to_addr = address(getBytes20(b, offset));
-        require(to_addr == rskContractAddress, "To address does not match");
+        require(to_addr == m_rsk_addr, "To address does not match");
 
         offset += 20;  
-        address dest_addr = uint(getBytes20(b, offset));
+        address dest_addr = address(getBytes20(b, offset));
 
         offset += 20;  
-        uint sbtc_amount = uint(getBytes32(b, offset));
+        uint ebtc_amount = uint(getBytes32(b, offset)); /* == sbtc */
    
+        require(EBTCToken(m_ebtc_token_addr).issueFreshTokens(dest_addr,
+                                                              ebtc_amount));
+        txn.issued = true;
 
+        delete m_query_map[query_id];
     }
 
     /* Called by user. 
@@ -94,18 +96,20 @@ contract StrideEthContract is mortal,usingOraclize {
     */
     function issueEBTC(bytes32 txn_hash, string json_request) public { 
         /* There should be enough balance for all Oraclize queries */
-        require(oraclize_getPrice("URL")  > this.balance, 
+        require(oraclize_getPrice("URL")  > address(this).balance, 
                                   "Oraclize query not send"); 
 
         /* Obtain transaction info from RSK */ 
+        bytes32 query_id;
         query_id = oraclize_query("URL", 
                                   "json(http://ropsten.bromleylabs.io).result",
                                   json_request); 
 
        if(m_fwd_txns[txn_hash].txn_hash != txn_hash) /* May already exist */ 
-           m_fwd_txns[txn_hash] = FwdTxn(txn_hash, query_id, false, false, 
-                                         false);
+           m_fwd_txns[txn_hash] = FwdTxn(txn_hash, false); 
+
        m_query_map[query_id] = txn_hash;
+    }
 
 }
 
