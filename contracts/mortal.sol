1322d0688b0c5daa95b991fbe240e13922b1d411
Bon Filey <puneet@outdu.com>
2018-06-20 12:37:14 +0530

    m_owner made public.

--- contracts/mortal.sol
+++ contracts/mortal.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.23;
 
 contract mortal {
-    address m_owner = msg.sender;  /* Whoever deploys this contract */ 
+    address public m_owner = msg.sender;  /* Whoever deploys this contract */ 
     function kill() public { if (msg.sender == m_owner) selfdestruct(m_owner); }
 }
