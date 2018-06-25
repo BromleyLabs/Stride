86469152e08aadabbfc394c8658f63b8f2b90948
Bon Filey <puneet@outdu.com>
2018-06-25 15:02:39 +0530

    WIP.

--- app/user/js/utils.js
+++ app/user/js/utils.js
@@ -1,8 +1,13 @@
-//require("./node_modules/web3/dist/web3.min.js");
-//require("./contract.js");
 
 function init_web3(ip) {
     return web3 = new Web3(new Web3.providers.HttpProvider(ip));
+}
+
+function get_pvt_key(key_contents, password) {
+    keyobj = JSON.parse(key_contents); 
+    from_addr = "0x" + keyobj.address
+    from_addr = web3.toChecksumAddress(from_addr);
+    return [keythereum.recover(password, keyobj), from_addr];
 }
 
 // input_id is element id for file input
@@ -11,65 +16,61 @@ function get_account_key(input_id, keystore_id) {
     var input = document.getElementById(input_id);
     var keystore = document.getElementById(keystore_id);
     input.addEventListener("change", function () {
-      if (this.files && this.files[0]) {
-        var key_file = this.files[0];
-        var reader = new FileReader();
-        reader.addEventListener('load', function (e) {
-           keystore.value = e.target.result;
-           console.log(keystore.value);
-        });
-        reader.readAsBinaryString(key_file)
-      }
+        if (this.files && this.files[0]) {
+            var key_file = this.files[0];
+            var reader = new FileReader();
+            reader.readAsText(key_file);
+            reader.addEventListener('load', function (e) {
+                keystore.value = e.target.result;
+                //console.log(keystore.value);
+            });
+        }
     });
 }
-/* 
-function deposit_sbtc(sbtc_amount, web3_eth, web3_rsk) {
-    web3.eth.getAccounts(function(err, accounts) {console.info(accounts);});
 
-        var stride_rsk = contract.at('0xc589638371dB9C7D00003a8E638A6b4910097996'); 
-        console.log(stride_rsk);
-          var sbtc = parseFloat(document.getElementById("sbtc").value); 
-            sbtc_wei = web3.toWei(sbtc, "ether"); 
-            var dest = document.getElementById("dest").value;
-            console.info("calling deposit");
-            document.getElementById("status").innerHTML = "Processing ..";
-            var account = web3.eth.accounts[0];
-            stride_rsk.depositSBTC(dest, {
-                    from: account, 
-                    gasPrice: "0.00000001", 
-                    gas: 4000000, 
-                    value: sbtc_wei
-                }, function(error, result) {
-                    if (error) {
-                        console.error(error);
-                        document.getElementById("status").innerHTML = "ERROR";
-                        err = true;
-                    }
-                    else { 
-                        txn_hash = result;
-                        console.info(txn_hash);
-                        wait_to_be_mined(txn_hash);
-                    } 
-                }
-            );
+function deposit_sbtc(key_contents, password, sbtc_amount, dest_addr, 
+                      contract_rsk, contract_eth, web3_eth, web3_rsk) {
+    sbtc_wei = web3.toWei(sbtc_amount, "ether"); 
+    [pvt_key, from_addr] = get_pvt_key(key_contents, password);
+    console.info("from addr:" + from_addr);
+    web3_rsk.eth.getTransactionCount(from_addr, function(err, nonce) {
+        var data = contract_rsk.depositSBTC(dest_addr).getData();  
+        var tx = new ethereumjs.Tx({
+            nonce: nonce,           
+            gasPrice: web3.toHex(web3.toWei('20', 'gwei')), 
+            gasLimit: 1000000, 
+            to: contract_rsk.address,
+            value: sbtc_wei,
+            data: data,
         });
+
+        console.log("Singing ..");
+        tx.sign(pvt_key);
+        var raw = '0x' + tx.serialize().toString('hex');
+        console.log("Calling send raw txn..");
+        web3_rsk.eth.sendRawTransaction(raw, function(err, txn_hash) {
+            console.log(txn_hash); 
+        });
+    });
 }
 
-    function wait_to_be_mined(txhash) {
-        console.info("Getting txn receipt");
-        web3.eth.getTransactionReceipt(txhash, function (err, result) {
-            if (!err && !result) {
-                // Try again with a bit of delay
-                setTimeout(function () {wait_to_be_mined (txhash) }, 2000);
-                console.info("Trying to get receipt again");
-            } else {
-                if (err)
-                    console.error("ERROR in Transaction"); 
-                else
-                    console.info("Transaction mined"); 
-            }
-        });
-    }
+/* 
+function wait_to_be_mined(txhash) {
+    console.info("Getting txn receipt");
+    web3.eth.getTransactionReceipt(txhash, function (err, result) {
+        if (!err && !result) {
+            // Try again with a bit of delay
+            setTimeout(function () {wait_to_be_mined (txhash) }, 2000);
+            console.info("Trying to get receipt again");
+        } else {
+            if (err)
+                console.error("ERROR in Transaction"); 
+            else
+                console.info("Transaction mined"); 
+        }
+    });
+}
 
 */
+
 
