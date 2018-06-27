170513ce1767d81fd0c4a66e9f2c7b98e6bd5a56
Bon Filey <puneet@outdu.com>
2018-06-27 06:08:32 +0530

    Complete forward transaction coded.

--- app/user/js/utils.js
+++ app/user/js/utils.js
@@ -3,8 +3,8 @@ function init_web3(ip) {
     return web3 = new Web3(new Web3.providers.HttpProvider(ip));
 }
 
-function get_pvt_key(key_contents, password) {
-    keyobj = JSON.parse(key_contents); 
+function get_pvt_key(key, password) {
+    keyobj = JSON.parse(key); 
     from_addr = "0x" + keyobj.address
     from_addr = web3.toChecksumAddress(from_addr);
     return [keythereum.recover(password, keyobj), from_addr];
@@ -28,17 +28,21 @@ function get_account_key(input_id, keystore_id) {
     });
 }
 
-function deposit_sbtc(key_contents, password, sbtc_amount, dest_addr, 
-                      contract_rsk, web3_rsk) {
-    sbtc_wei = web3.toHex(web3.toWei(sbtc_amount, "ether")); 
-    [pvt_key, from_addr] = get_pvt_key(key_contents, password);
+/* TODO: Revert sendRawTransaction to callback
+   w3:  web3 instance of RSK node (e.g. http://server:4444) 
+   rsk_key: User keystore file for RSK account
+ */ 
+function deposit_sbtc(rsk_key, password, sbtc_amount, dest_addr, 
+                      contract_rsk, w3) {
+    sbtc_wei = w3.toHex(w3.toWei(sbtc_amount, "ether")); 
+    [pvt_key, from_addr] = get_pvt_key(rsk_key, password);
     console.info("from addr:" + from_addr);
-    web3_rsk.eth.getTransactionCount(from_addr, function(err, nonce) {
+    w3.eth.getTransactionCount(from_addr, function(err, nonce) {
         console.info(nonce);
         var data = contract_rsk.depositSBTC.getData(dest_addr);  
         var tx = new ethereumjs.Tx({
             nonce: nonce,           
-            gasPrice: web3.toHex(web3.toWei('0', 'gwei')), 
+            gasPrice: w3.toHex(w3.toWei('0', 'gwei')), 
             gasLimit: 4000000, 
             to: contract_rsk.address,
             value: sbtc_wei,
@@ -50,13 +54,13 @@ function deposit_sbtc(key_contents, password, sbtc_amount, dest_addr,
         var raw = '0x' + tx.serialize().toString('hex');
         console.info(raw);
         console.info("Calling send raw txn..");
-        txn_hash = web3_rsk.eth.sendRawTransaction(raw);
+        txn_hash = w3.eth.sendRawTransaction(raw); /* TODO: See above */
         if (txn_hash == null) { 
             console.error("Error sending Txn");
             return;
         }
         console.log(txn_hash); 
-        wait_to_be_mined(web3_rsk, txn_hash);
+        wait_to_be_mined(w3, txn_hash);
    });
 }
 
@@ -64,7 +68,7 @@ function wait_to_be_mined(w3, txn_hash) {
     console.info("Getting txn receipt");
     w3.eth.getTransactionReceipt(txn_hash, function (err, result) {
         if (!err && !result) {
-            // Try again with a bit of delay
+            /* Try again with a bit of delay */
             setTimeout(function () {wait_to_be_mined(w3, txn_hash) }, 3000);
         } else {
             if (err)
@@ -77,13 +81,69 @@ function wait_to_be_mined(w3, txn_hash) {
     });
 }
 
+/* Wait for some n confirmations and then call Ethereum contract method to 
+ * to issue EBTC 
+ */
 function wait_for_confirmations(w3, start_block, n) {
     console.info("Waiting for enough confirmations");
     curr_block = w3.eth.blockNumber; 
     if ((curr_block - start_block) <= n) {
-        setTimeout(function () {wait_for_confirmations(w3, start_block, n) }, 3000);
+        setTimeout(function () {wait_for_confirmations(w3, start_block, n) }, 
+                   3000);
     }
     else {
         console.info(n + " confirmations done");
+        issue_ebtc(web3_eth, contract_eth, rsk_key, password,  txn_hash, 
+                    sbtc_amount); 
     }
+}
+
+function wait_for_event(contract) {
+    contract.EBTCIssued(function(err, result) {
+        if (err) 
+            console.error('Error in receiving event');
+        else
+            console.info('EBTCIssued event received');
+            console.info('Transaction completed');
+    });
+}
+
+/* This function is called on the Ethereum contract 
+   w3:  web3 instance of Eth node (e.g. http://server:8545) 
+   eth_key: User keystore file for RSK account
+   TODO: Revert sendRawTransaction to callback
+*/
+function issue_ebtc(w3, contract_eth, eth_key, password,  deposit_txn_hash, 
+                    ebtc_amount) {
+    ebtc_wei = w3.toHex(w3.toWei(ebtc_amount, "ether")); 
+    [pvt_key, from_addr] = get_pvt_key(eth_key, password);
+    js = JSON.stringify({"jsonrpc" : "2.0", "id" : 0,
+                         "method" : "eth_getTransactionByHash",
+                         "params" : deposit_txn_hash}); 
+    w3.eth.getTransactionCount(from_addr, function(err, nonce) {
+        console.info(nonce);
+        var data = contract_eth.issueEBTC.getData(deposit_txn_hash, js);  
+        var tx = new ethereumjs.Tx({
+            nonce: nonce,           
+            gasPrice: w3.toHex(w3.toWei('20', 'gwei')), 
+            gasLimit: 4000000, 
+            to: contract_eth.address,
+            value: 0,
+            data: data,
+        });
+
+        console.info("Signing ..");
+        tx.sign(pvt_key);
+        var raw = '0x' + tx.serialize().toString('hex');
+        console.info(raw);
+        console.info("Calling send raw txn..");
+        txn_hash = w3.eth.sendRawTransaction(raw); /* TODO: See above */
+        if (txn_hash == null) { 
+            console.error("Error sending Txn");
+            return;
+        }
+        console.log(txn_hash); 
+        wait_for_event(contract_eth); 
+   });
+  
 }
