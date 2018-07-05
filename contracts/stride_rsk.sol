1c282e1ecf5ccb1eba14147f15ab8a702732d9c3
Bon Filey <puneet@outdu.com>
2018-07-05 17:16:37 +0530

    rev_redeem() added. WIP.

--- contracts/stride_rsk.sol
+++ contracts/stride_rsk.sol
@@ -28,9 +28,10 @@ contract StrideRSKContract is mortal {
 
     mapping (uint => ForwardTxn) public m_fwd_txns;  
     /* Eth txn hash => true   */
-    mapping (uint => bool) public m_sbtc_issued;
+    mapping (bytes32 => bool) public m_sbtc_issued;
 
     address public m_custodian_rsk;   
+    address public m_eth_contract_addr;
     address public m_eth_proof_addr; /* Address of EthProof contract */
     uint public m_locked_sbtc = 0;
     uint public m_sbtc_lock_interval = 100;  /* In blocks. */
@@ -47,6 +48,11 @@ contract StrideRSKContract is mortal {
     function set_eth_proof_addr(address addr) public {
         require(msg.sender == m_owner);
         m_eth_proof_addr = addr;
+    }
+
+    function set_eth_contract_addr(address addr) public {
+        require(msg.sender == m_owner);
+        m_eth_contract_addr = addr;
     }
 
     function set_lock_interval(uint nblocks) public {
@@ -103,8 +109,8 @@ contract StrideRSKContract is mortal {
 
     /** Called by the user, this function redeems SBTC to the destination 
         address specified on Ethereum side.  The user provides proof of 
-        Ethereum transaction receipt which is verified in this function. Logs
-        of transaction receipt contain user RSK destination address and 
+        Ethereum transaction receipt which is verified in this function. Reads
+        logs in transaction receipt containing user RSK destination address and 
         SBTC amount (refer to Ethereum contract). 
         @param rlp_txn_receipt bytes The full transaction receipt structure 
         @param block_hash bytes32 Hash of the block in which Ethereum 
@@ -114,11 +120,25 @@ contract StrideRSKContract is mortal {
      */
     function rev_redeem(bytes rlp_txn_receipt, bytes32 block_hash, bytes path,
                         bytes rlp_parent_nodes) public {
+        require(m_sbtc_issued[keccak256(rlp_txn_receipt)] != true, 
+                "SBTC already issued for this transaction");
         require(EthProof(m_eth_proof_addr).check_receipt_proof(rlp_txn_receipt,
-                block_hash, path, rlp_parent_nodes));         
+                block_hash, path, rlp_parent_nodes), "Incorrect proof");         
+        RLP.RLPItem memory item = RLP.toRLPItem(rlp_txn_receipt);
+        RLP.RLPItem[] memory fields = RLP.toList(item);
+        uint status = uint(RLP.toUint(fields[0])); /* First field is status */
+        require(status > 0);
+     
+        RLP.RLPItem[] memory logs = RLP.toList(fields[3]); /* Logs */
+        RLP.RLPItem[] memory log_fields = RLP.toList(logs[0]); /* Only 1 log */
+        require(RLP.toAddress(log_fields[0]) == m_eth_contract_addr);
 
-        /* TODO: Check status flag in receipt */ 
-        /* TODO: Read dest address and ebtc */
+        RLP.RLPItem[] memory event_params = RLP.toList(log_fields[2]);
+        address dest_addr = RLP.toAddress(event_params[0]);
+        uint sbtc_amount = RLP.toUint(event_params[1]);
+        
+        dest_addr.transfer(sbtc_amount); 
     
+        m_sbtc_issued[keccak256(rlp_txn_receipt)] = true; 
     } 
 }
