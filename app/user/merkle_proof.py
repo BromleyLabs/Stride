02e3bc1bbdc0404b33e852d86ee3f14017e26889
Bon Filey <puneet@outdu.com>
2018-07-09 22:49:30 +0530

    Code for finding the stack related to a path.

--- app/user/merkle_proof.py
+++ app/user/merkle_proof.py
@@ -4,6 +4,7 @@ from hexbytes import HexBytes
 import rlp
 from web3.auto import w3
 from trie import Trie 
+import trie.utils.nibbles as nibbles
 
 def int_to_buf(a):
      return HexBytes('%s' % hex(a))
@@ -18,14 +19,15 @@ def encode_logs(logs):
     return encoded_logs
 
 def get_rlp_receipt(r): # Receipt
-    if r.status == 0:
+    if r.status == 0: # Ref Ethereum implementation: core/types/receipt.go
         status = b''
     else:
-        status = int_to_buf(r['status']) 
-    cummulative_gas = int_to_buf(r['cumulativeGasUsed'])
-    bloom_filter = r['logsBloom'] 
-    logs = encode_logs(r['logs']) 
-    raw_receipt = rlp.encode([status, cummulative_gas, bloom_filter, logs])
+        status = int_to_buf(r.status)  # b'\x01'
+
+    raw_receipt = rlp.encode([status, 
+                              int_to_buf(r.cumulativeGasUsed),
+                              r.logsBloom, 
+                              encode_logs(r.logs)])
     return raw_receipt
 
 def print_node(node):
@@ -40,22 +42,33 @@ def build_receipt_proof(w3, receipt_trie, txn_hash):
         sibling_receipt = w3.eth.getTransactionReceipt(tr.hex())
         value =  get_rlp_receipt(sibling_receipt) 
         receipt_trie.set(path, value)
+        if i == receipt.transactionIndex:
+            rlp_txn_receipt = value  # We are interested in this txn 
  
-    print('Original receipts Root = %s' % (block.receiptsRoot).hex()) 
-    print('Root hash = %s' % HexBytes(receipt_trie.root_hash).hex()) 
-    
-    #root_node = receipt_trie.get_node(receipt_trie.root_hash)
-    #print_node(root_node)
-    #second_node = receipt_trie.get_node(root_node[0])
-    #print_node(second_node)
-    #third_node = receipt_trie.get_node(second_node[1])
-    #print_node(third_node)
+    txn_path = rlp.encode(receipt.transactionIndex)
+    parent_nodes = []
+    t = receipt_trie
+    parent_nodes.append(t.root_node)
+    node = t.root_node
+    nibs = nibbles.bytes_to_nibbles(txn_path)
+    for nib in nibs: 
+        next_node = rlp.decode(t.db[node[nib]])    
+        parent_nodes.append(next_node)
+        node = next_node
+
+    rlp_parent_nodes = rlp.encode(parent_nodes)
+    print('Calculated hash = %s' % HexBytes(w3.sha3(rlp.encode(t.root_node))).hex())
+    print('Receipts root = %s' % HexBytes(block.receiptsRoot).hex()) 
+
+    return rlp_txn_receipt, receipt.blockHash, txn_path, rlp_parent_nodes
+
 
 if __name__== '__main__':
     from web3.auto import w3    
    
     receipts_trie = Trie(db = {})
     txn_hash = '0x3606ff2b53d5bbd22f6c35473aff06bbde770ed4ce5c9c6f969736206c06b94b'
-    #txn_hash = '0x7c85585eaf277bf4933f9702930263a451d62fba664be9c69f5cf891ba226e4a'
-    build_receipt_proof(w3, receipts_trie, txn_hash) 
+    r, h, p, nodes = build_receipt_proof(w3, receipts_trie, txn_hash) 
 
+        
+        
