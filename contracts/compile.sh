c90af6ea8bc75ffb40e9d964c37c88d281f9a8a4
Bon Filey <bonfiley@gmail.com>
2018-07-23 21:22:59 +0530

    Few compile time options removed as not really needed

--- contracts/compile.sh
+++ contracts/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOLC_PATH=/usr/bin
-OPTIONS="--bin --abi --optimize --opcodes --overwrite"
+OPTIONS="--bin --abi --optimize"
 rm -rf target
 $SOLC_PATH/solc -o target $OPTIONS stride_eth.sol stride_rsk.sol eth_proof.sol 
 
